1c1a31f773a8ea4b1480447cb524c6e2dcf4fc3f
gabrielemasciulli <gmasciu@gmail.com>
2024-12-10 18:33:02 +0100

    Refactor SalesProductsPage component: updated import statements and simplified JSX syntax for better readability

--- src/components/sales-products.tsx
+++ src/components/sales-products.tsx
@@ -1,28 +1,27 @@
-"use client";
+'use client'
 
-import { redirect } from "next/navigation";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import Link from "next/link";
-import products from "@/lib/data/productsData"; // Import dei dati
+} from '@/components/ui/card'
+import Link from 'next/link'
+import products from '@/lib/data/productsData' // Import dei dati
 
 export default function SalesProductsPage() {
   return (
     <div>
-      <h1 className="text-4xl text-center p-10">Sales Products</h1>
-      <div className="grid grid-cols-4 gap-4">
-        {products.map((product) => (
+      <h1 className='text-4xl text-center p-10'>Sales Products</h1>
+      <div className='grid grid-cols-4 gap-4'>
+        {products.map(product => (
           <Link
             key={product.id}
             href={`products/product/${product.id}`}
             passHref
           >
-            <Card className="mb-4">
+            <Card className='mb-4'>
               <CardHeader>
                 <CardTitle>{product.name}</CardTitle>
                 <CardDescription>{product.quantityAvailable}</CardDescription>
@@ -30,7 +29,7 @@ export default function SalesProductsPage() {
               <CardContent>
                 <p>Prezzo al dettaglio: {product.price}</p>
                 <p>Regione di provenienza: {product.region}</p>
-                {product.agents.map((agent) => (
+                {product.agents.map(agent => (
                   <p key={agent}>Agente: {agent}</p>
                 ))}
               </CardContent>
@@ -39,5 +38,5 @@ export default function SalesProductsPage() {
         ))}
       </div>
     </div>
-  );
+  )
 }
