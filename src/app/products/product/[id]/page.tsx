93854b634718af61930d9fdd6d71dabd3d628ed2
gabrielemasciulli <gmasciu@gmail.com>
2024-12-10 18:37:51 +0100

    Refactor Product page: streamline import statements, update async parameter handling, and enhance JSX syntax for improved readability and maintainability

--- src/app/products/product/[id]/page.tsx
+++ src/app/products/product/[id]/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-
-import { TrendingUp } from "lucide-react";
-import {
-  Bar,
-  BarChart,
-  CartesianGrid,
-  LabelList,
-  XAxis,
-  YAxis,
-} from "recharts";
+import { TrendingUp } from 'lucide-react'
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis, YAxis } from 'recharts'
 
 import {
   Card,
@@ -17,44 +8,48 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
+} from '@/components/ui/card'
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
-} from "@/components/ui/chart";
+} from '@/components/ui/chart'
 const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
-];
+  { month: 'January', desktop: 186, mobile: 80 },
+  { month: 'February', desktop: 305, mobile: 200 },
+  { month: 'March', desktop: 237, mobile: 120 },
+  { month: 'April', desktop: 73, mobile: 190 },
+  { month: 'May', desktop: 209, mobile: 130 },
+  { month: 'June', desktop: 214, mobile: 140 },
+]
 
 const chartConfig = {
   desktop: {
-    label: "Desktop",
-    color: "hsl(var(--chart-1))",
+    label: 'Desktop',
+    color: 'hsl(var(--chart-1))',
   },
   mobile: {
-    label: "Mobile",
-    color: "hsl(var(--chart-2))",
+    label: 'Mobile',
+    color: 'hsl(var(--chart-2))',
   },
   label: {
-    color: "hsl(var(--background))",
+    color: 'hsl(var(--background))',
   },
-} satisfies ChartConfig;
+} satisfies ChartConfig
 
-import products from "@/lib/data/productsData";
+import products from '@/lib/data/productsData'
 
-export default function Product({ params }: { params: { id: string } }) {
-  const { id: productId } = params; // Estrae l'id dall'URL
-  const product = products.find((product) => product.id === productId); // Trova il progetto con l'id corrispondente
+export default async function Product({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const id = (await params).id
+  const product = products.find(product => product.id === id) // Trova il progetto con l'id corrispondente
 
   if (!product) {
-    return <p>Product not found</p>; // Gestione di progetti non esistenti
+    return <p>Product not found</p> // Gestione di progetti non esistenti
   }
 
   return (
@@ -66,8 +61,8 @@ export default function Product({ params }: { params: { id: string } }) {
             <p>Prezzo: {product.price}</p>
             <p>Regione: {product.region}</p>
             <p>
-              Agenti:{" "}
-              {product.agents.map((agent) => (
+              Agenti:{' '}
+              {product.agents.map(agent => (
                 <p key={agent}>{agent}</p>
               ))}
             </p>
@@ -79,59 +74,59 @@ export default function Product({ params }: { params: { id: string } }) {
             <BarChart
               accessibilityLayer
               data={chartData}
-              layout="vertical"
+              layout='vertical'
               margin={{
                 right: 16,
               }}
             >
               <CartesianGrid horizontal={false} />
               <YAxis
-                dataKey="month"
-                type="category"
+                dataKey='month'
+                type='category'
                 tickLine={false}
                 tickMargin={10}
                 axisLine={false}
-                tickFormatter={(value) => value.slice(0, 3)}
+                tickFormatter={value => value.slice(0, 3)}
                 hide
               />
-              <XAxis dataKey="desktop" type="number" hide />
+              <XAxis dataKey='desktop' type='number' hide />
               <ChartTooltip
                 cursor={false}
-                content={<ChartTooltipContent indicator="line" />}
+                content={<ChartTooltipContent indicator='line' />}
               />
               <Bar
-                dataKey="desktop"
-                layout="vertical"
-                fill="var(--color-desktop)"
+                dataKey='desktop'
+                layout='vertical'
+                fill='var(--color-desktop)'
                 radius={4}
               >
                 <LabelList
-                  dataKey="month"
-                  position="insideLeft"
+                  dataKey='month'
+                  position='insideLeft'
                   offset={8}
-                  className="fill-[--color-label]"
+                  className='fill-[--color-label]'
                   fontSize={12}
                 />
                 <LabelList
-                  dataKey="desktop"
-                  position="right"
+                  dataKey='desktop'
+                  position='right'
                   offset={8}
-                  className="fill-foreground"
+                  className='fill-foreground'
                   fontSize={12}
                 />
               </Bar>
             </BarChart>
           </ChartContainer>
         </CardContent>
-        <CardFooter className="flex-col items-start gap-2 text-sm">
-          <div className="flex gap-2 font-medium leading-none">
-            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        <CardFooter className='flex-col items-start gap-2 text-sm'>
+          <div className='flex gap-2 font-medium leading-none'>
+            Trending up by 5.2% this month <TrendingUp className='h-4 w-4' />
           </div>
-          <div className="leading-none text-muted-foreground">
+          <div className='leading-none text-muted-foreground'>
             Showing total visitors for the last 6 months
           </div>
         </CardFooter>
       </Card>
     </div>
-  );
+  )
 }
