b582b8081e2af82ee2551ced80bfd83a0561461b
gabrielemasciulli <gmasciu@gmail.com>
2024-12-11 18:13:41 +0100

    Sistemato padding filter bar per consistenza con la dashboard principale.

--- src/components/ui/filter-bar.tsx
+++ src/components/ui/filter-bar.tsx
@@ -19,7 +19,7 @@ export default function FilterBar() {
   })
 
   return (
-    <div className='flex flex-col  px-4 py-4 space-y-4 lg:flex-row lg:space-x-4 lg:space-y-0'>
+    <div className='flex flex-col px-8 py-4 space-y-4 lg:flex-row lg:space-x-4 lg:space-y-0'>
       <div className='flex items-center space-x-4'>
         <DatePickerWithRange date={dateRange} setDate={setDateRange} />
         <Select defaultValue='all'>
